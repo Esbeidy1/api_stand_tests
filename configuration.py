ae94e751bf8ee0ffa3f645b9f9d0b9f7df19f420
Esbeidy Garcia <esbeidy@Esbeidys-MacBook-Pro.local>
2024-04-30 18:33:44 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://cnt-1addb118-a9cb-4dfa-ae75-554c2366d27f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
+
